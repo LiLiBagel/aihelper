faa36343e116441aaf29b4a8211d5c73a19a779d
LiLiBagel <zhuastor@gmail.com>
2025-04-21 11:35:30 +0800

    Update gspread_helper.py

--- gspread_helper.py
+++ gspread_helper.py
@@ -2,7 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("englishhelper-457503-00afcaa9c20c.json", scope)
 client = gspread.authorize(creds)
 
 SHEET_NAME = "DailyVocab"
